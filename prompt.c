15afbc1d73e78b514ba1863a410229f4cb7e87d9
Vivek Nair <viveknair360@gmail.com>
2021-04-25 11:13:39 -0400

    Minor+1: echoes input, multiline input enabled, exit command terminates shell. Yaygit add .!

---
+++ prompt.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include "vshell.h"
+
+
+/* Displayed when shell is waiting for user command */
+void print_prompt_1(void)
+{
+    fprintf(stderr, "$ ");
+}
+
+/* Displayed when shell is waiting for multi-line user command */
+void print_prompt_2(void)
+{
+    fprintf(stderr, "> ");
+}
