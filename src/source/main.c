0973f05641a9e178ec1c771f1da29e7681db560c
VivekNair360 <viveknair360@gmail.com>
2021-05-02 09:57:05 -0400

    Metadata: Added documentation and updated Makefile with "clean"

--- src/source/main.c
+++ src/source/main.c
@@ -4,50 +4,89 @@
 #include <string.h>
 #include "../headers/vshell.h"
 
-#define TRUE (1)
-#define NORM_BUFF_LEN_BYTES (1024)
+#define EQUAL 0
+#define TRUE 1
+#define NORM_BUFF_LEN_BYTES 1024
+#define BUFF_ALLOC_FAIL "error: failed to allocate buffer: %s\n"
 
 int main (int argc, char ** argv)
 {
     char * cmd;
+
+    /* 
+        REPL (Read, Eval, Print, Loop)
+    */
     
     do
     {
+
         print_prompt_1();
         
+        /* 
+            Read user input 
+        */
+
         cmd = read_cmd();
+
+        /*
+            Evaluate user input
+        */
 
         if (!cmd)
         {
             exit(EXIT_SUCCESS);
         }
 
-        if (cmd[0] == '\0' || strcmp(cmd, "\n") == 0)
+        if (cmd[0] == '\0' || strcmp(cmd, "\n") == EQUAL)
         {
             free(cmd);
             continue;
         }
 
-        if (strcmp(cmd, "exit\n") == 0)
+        if (strcmp(cmd, "exit\n") == EQUAL)
         {
             free(cmd);
             break;
         }
 
+        /*
+            Print result of command
+        */
+
         printf("%s\n", cmd);
 
         free(cmd);
+
+        /*
+            Loop
+        */
 
     } while (TRUE);
     
     exit(EXIT_SUCCESS);
 }
 
+/*
+    Reads user input from stdin into NORM_BUFF_LEN_BYTES chunks, 
+    and stores this input in a buffer
+*/
+
 char * read_cmd (void)
 {
+    /*
+        Declare buffer, 
+        pointer to user input,
+        and length of pointer variables
+    */
+
     char buff[NORM_BUFF_LEN_BYTES];
     char * ptr = NULL;
     char ptrlen = 0;
+
+    /*
+        Read NORM_BUFF_LEN_BYTES from stdin into the buffer,
+        and copy the contents of the buffer to a character array,
+        and return a pointer to the character array */
 
     while (fgets(buff, NORM_BUFF_LEN_BYTES, stdin))
     {
@@ -74,7 +113,7 @@ char * read_cmd (void)
 
         if (!ptr)
         {
-            fprintf(stderr, "error: failed to allocate buffer: %s\n", strerror(errno));
+            fprintf(stderr, BUFF_ALLOC_FAIL, strerror(errno));
             return NULL;
         }
 
--- src/source/prompt.c
+++ src/source/prompt.c
@@ -1,15 +1,20 @@
 #include <stdio.h>
 #include "../headers/vshell.h"
 
+#define NORM_PROMPT "$ "
+#define MULTI_LINE_PROMPT "> "
+
 
 /* Displayed when shell is waiting for user command */
+
 void print_prompt_1(void)
 {
-    fprintf(stderr, "$ ");
+    fprintf(stderr, NORM_PROMPT);
 }
 
 /* Displayed when shell is waiting for multi-line user command */
+
 void print_prompt_2(void)
 {
-    fprintf(stderr, "> ");
+    fprintf(stderr, MULTI_LINE_PROMPT);
 }
